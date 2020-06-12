633762998c9452185ecc81ec0626e750210f3d3d
Paul Sztajer <talk@pdyxs.wtf>
2020-06-12 23:25:44 +1000

    only importing icons i use

--- src/components/Game/Icons/index.tsx
+++ src/components/Game/Icons/index.tsx
@@ -4,17 +4,30 @@ import './Icons.scss';
 import { IconProps } from "../Pieces";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas as fasp } from '@fortawesome/pro-solid-svg-icons';
-import { far as farp } from '@fortawesome/pro-regular-svg-icons';
+import { faSlash, faSquare, faSkullCrossbones, faBolt, faUserAstronaut, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { faWindowClose, faClone } from '@fortawesome/free-regular-svg-icons';
+import { faPlusOctagon, faTombstone, faSnooze } from '@fortawesome/pro-solid-svg-icons';
+import { faTint, faUnlink } from '@fortawesome/pro-regular-svg-icons';
 import { faMediumM } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas);
-library.add(far);
-library.add(fasp);
-library.add(farp);
-library.add(faMediumM);
+library.add(
+  faSquare,
+  faSkullCrossbones,
+  faBolt,
+  faUserAstronaut,
+  faWrench,
+  faPlusOctagon,
+  faSlash,
+  faSnooze,
+  faTombstone,
+
+  faWindowClose,
+  faTint,
+  faClone,
+  faUnlink,
+
+  faMediumM
+);
 
 export const FuelIcon : React.FC<IconProps> = ({className}) =>
   <FontAwesomeIcon className={className} icon={['fas', 'bolt']} />
